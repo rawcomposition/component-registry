9562880185aff4691842ef83da61d281b47cc9c1
Adam Jackson <akjackson1@gmail.com>
2025-05-16 10:43:39 -0700

    BUg fix

--- components/open-in-v0-button.tsx
+++ components/open-in-v0-button.tsx
@@ -12,7 +12,7 @@ export function OpenInV0Button({ name, className }: { name: string } & React.Com
       asChild
     >
       <a
-        href={`https://v0.dev/chat/api/open?url=${process.env.NEXT_PUBLIC_BASE_URL}/registry/${name}.json`}
+        href={`https://v0.dev/chat/api/open?url=${process.env.NEXT_PUBLIC_BASE_URL}/r/${name}.json`}
         target="_blank"
         rel="noreferrer"
       >
